9c8ad0fd5252098862ec6fd37fe57924a2dfe953
玊烨 <207279604@qq.com>
2023-03-01 23:45:58 +0800

    小说设定更新

---
+++ personal_tool/novel/event_creator/entity/role/苏木槿.py
@@ -0,0 +1,12 @@
+from personal_tool.novel.event_creator.base.role import Role
+
+
+class SuMuJin(Role):
+
+    def set_role_info(self):
+        """设置角色信息"""
+        self.name = "苏木槿"
+        self.persona = """
+        十七岁，高二。
+        两年前与东方灵相遇，之后以插班生身份进入当前的学校。（埋伏笔，苏木槿在学校是校霸，引出设定东方灵是学校校董）
+        """
