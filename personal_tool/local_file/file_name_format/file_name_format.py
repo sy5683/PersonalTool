e857cc1273591a4442b23531be1b2b5b4a9b083f
玊烨 <207279604@qq.com>
2023-03-12 13:08:46 +0800

    实现重命名动漫的方法、并将重命名动漫与重命名漫画的复用方法分割出来。

--- personal_tool/local_file/file_name_format/file_name_format.py
+++ personal_tool/local_file/file_name_format/file_name_format.py
@@ -1,54 +1,22 @@
-import os.path
-import os.path
-import re
-import tkinter
-from tkinter import filedialog
-
 import win32api
 
 from base.tool_base import ToolBase
+from personal_tool.local_file.file_name_format.cache.path_cache import PathCache
+from personal_tool.local_file.file_name_format.feature.format_feature import FormatFeature
 
 
 class FileNameFormat(ToolBase):
     """文件名格式化"""
 
     def __init__(self):
-        tkinter.Tk().withdraw()  # 隐藏tk窗口
-        self.directory_path = filedialog.askdirectory()
+        self.directory_path = PathCache.get_directory_path()  # 初始化获取文件路径
 
     def main(self, function=None, **kwargs):
-        if function:
-            function(self, **kwargs)
-        # 打开结果文件夹
-        win32api.ShellExecute(0, "open", self.directory_path, "", "", 1)
-
-    def manga_format(self, suffix: str = None, start_number: int = None):
-        """格式化漫画"""
-        manga_names = sorted(os.listdir(self.directory_path), key=lambda x: int(re.sub(r'\D+', '', x)))
-        for manga_name in manga_names:
-            manga_stem, manga_suffix = os.path.splitext(manga_name)
-            # 1) 更改文件名
-            if start_number is not None:
-                manga_stem = f"temp_{str(start_number).zfill(3)}"
-                start_number += 1
-            # 2) 更改文件后缀
-            if suffix:
-                manga_suffix = suffix if suffix.startswith(".") else f".{suffix}"
-            else:
-                if re.search(r"\.webp$", manga_suffix):
-                    manga_suffix = ".jpg"
-            # 3) 重命名文件
-            new_manga_name = f"{manga_stem}{manga_suffix}"
-            if manga_name != new_manga_name:
-                self._rename(manga_name, new_manga_name)
-        for manga_name in sorted(os.listdir(self.directory_path)):
-            self._rename(manga_name, re.sub(r"^temp_", "", manga_name))
-
-    def _rename(self, file_name_from: str, file_name_to: str):
-        """文件重命名"""
-        file_path_from = os.path.join(self.directory_path, file_name_from)
-        file_path_to = os.path.join(self.directory_path, file_name_to)
-        os.rename(file_path_from, file_path_to)
+        if self.directory_path:
+            if function:
+                function(**kwargs)
+            # 打开结果文件夹
+            win32api.ShellExecute(0, "open", str(self.directory_path), "", "", 1)
 
     def _directory_path_verification(self):
         """文件校验"""
@@ -56,4 +24,5 @@ class FileNameFormat(ToolBase):
 
 if __name__ == '__main__':
     file_name_format = FileNameFormat()
-    file_name_format.main(FileNameFormat.manga_format, start_number=1)
+    file_name_format.main(FormatFeature.anime_format)
+    # file_name_format.main(FormatFeature.manga_format, start_number=1)
