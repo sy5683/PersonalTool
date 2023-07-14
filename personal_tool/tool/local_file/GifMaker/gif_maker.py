b43103b973092f20e4ed2bc715d6f94d66b92d19
玊烨 <207279604@qq.com>
2023-07-14 09:55:06 +0800

    整理代码

--- personal_tool/tool/local_file/GifMaker/gif_maker.py
+++ personal_tool/tool/local_file/GifMaker/gif_maker.py
@@ -1,12 +1,8 @@
-import uuid
-
-import cv2
-import imageio
 import win32api
-from PIL import Image
 
-from personal_tool.base.tool_base import ToolBase
 from gif_maker.feature.image_feature import ImageFeature
+from gif_maker.make_rotate_gif import MakeRotateGif
+from personal_tool.base.tool_base import ToolBase
 
 
 class GifMaker(ToolBase):
@@ -17,31 +13,12 @@ class GifMaker(ToolBase):
 
     def main(self, function=None, **kwargs):
         if function and ImageFeature.get_image_paths():
-            function(self, **kwargs)
+            kwargs['duration'] = self.duration
+            function(**kwargs)
             # 打开结果文件夹
             win32api.ShellExecute(0, "open", ImageFeature.get_save_path(), "", "", 1)
-
-    def make_rotate_gif(self, angle: int, func_way='cv2'):
-        """生成旋转的gif"""
-        for image_path in ImageFeature.get_image_paths():
-            rotate_images = []
-            gif_path = ImageFeature.get_save_path(f"temp_gif_{str(uuid.uuid4())}.gif")
-
-            if func_way == "cv2":
-                # TODO cv2实现生成的gif背景不透明，需要寻找优化方案
-                image = cv2.imread(image_path)
-                for times in range(360 // angle):
-                    rotate_image = ImageFeature.image_rotate(image, angle, times)
-                    rotate_images.append(rotate_image)
-                imageio.mimsave(gif_path, rotate_images, 'GIF', duration=self.duration)
-            else:
-                # TODO PIL实现旋转时会出现严重的锯齿，仅记录，之后废弃
-                image = Image.open(image_path)
-                for times in range(360 // angle):
-                    rotate_images.append(image.rotate(angle * times))
-                image.save(gif_path, save_all=True, append_images=rotate_images[1:], duration=self.duration)
 
 
 if __name__ == '__main__':
     gif_maker = GifMaker()
-    gif_maker.main(GifMaker.make_rotate_gif, angle=2)
+    gif_maker.main(MakeRotateGif.make_rotate_gif, angle=2)
---
+++ personal_tool/tool/local_file/GifMaker/gif_maker/make_rotate_gif.py
@@ -0,0 +1,31 @@
+import uuid
+
+import cv2
+import imageio
+from PIL import Image
+
+from .feature.image_feature import ImageFeature
+
+
+class MakeRotateGif:
+
+    @staticmethod
+    def make_rotate_gif(angle: int, duration: float, func_way='cv2'):
+        """生成旋转的gif"""
+        for image_path in ImageFeature.get_image_paths():
+            rotate_images = []
+            gif_path = ImageFeature.get_save_path(f"temp_gif_{str(uuid.uuid4())}.gif")
+
+            if func_way == "cv2":
+                # TODO cv2实现生成的gif背景不透明，需要寻找优化方案
+                image = cv2.imread(image_path)
+                for times in range(360 // angle):
+                    rotate_image = ImageFeature.image_rotate(image, angle, times)
+                    rotate_images.append(rotate_image)
+                imageio.mimsave(gif_path, rotate_images, 'GIF', duration=duration)
+            else:
+                # TODO PIL实现旋转时会出现严重的锯齿，仅记录，之后废弃
+                image = Image.open(image_path)
+                for times in range(360 // angle):
+                    rotate_images.append(image.rotate(angle * times))
+                image.save(gif_path, save_all=True, append_images=rotate_images[1:], duration=duration)
