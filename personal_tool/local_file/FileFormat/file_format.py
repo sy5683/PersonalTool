f749b0d2a72ed5be579ca57e7bc1dc090c124d75
玊烨 <207279604@qq.com>
2023-07-19 11:22:34 +0800

    整理代码

--- personal_tool/local_file/FileFormat/file_format.py
+++ personal_tool/local_file/FileFormat/file_format.py
@@ -1,8 +1,15 @@
+from enum import Enum
+
 import win32api
 
 from file_format.anime_format import AnimeFormat
 from file_format.feature.file_feature import FileFeature
 from file_format.manga_format import MangaFormat
+
+
+class Operations(Enum):
+    anime_format = AnimeFormat.anime_format
+    manga_format = MangaFormat.manga_format
 
 
 class FileFormat:
@@ -17,5 +24,5 @@ class FileFormat:
 
 if __name__ == '__main__':
     file_format = FileFormat()
-    # file_format.main(AnimeFormat.anime_format)
-    file_format.main(MangaFormat.manga_format, start_number=1)
+    # file_format.main(Operations.anime_format)
+    file_format.main(Operations.manga_format, start_number=1)
