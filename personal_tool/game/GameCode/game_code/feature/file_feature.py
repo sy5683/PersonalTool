ec551f517e40fa026e8e07213493c192a49d0919
玊烨 <207279604@qq.com>
2023-08-04 17:18:55 +0800

    整理代码

--- personal_tool/game/GameCode/game_code/feature/file_feature.py
+++ personal_tool/game/GameCode/game_code/feature/file_feature.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 class FileFeature:
 
+    @classmethod
+    def to_database_path(cls, database_name: str = '') -> Path:
+        return cls._to_project_path().joinpath(f"database\\{database_name}")
+
     @staticmethod
-    def to_database_path(database_name: str = '') -> Path:
-        return Path(__file__).parent.parent.joinpath(f"database\\{database_name}")
+    def _to_project_path() -> Path:
+        return Path(__file__).parent.parent
