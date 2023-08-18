54a6f1d0fb7d90282a75b20c2fbd5c12a09fabfa
玊烨 <207279604@qq.com>
2023-08-18 16:51:41 +0800

    添加nlp工具

---
+++ personal_tool/ai/NLP/nlp/feature/cut/entity/jieba_cut.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import jieba
+
+
+class JiebaCut:
+
+    def __init__(self):
+        # 分词器添加自定义词语
+        jieba.add_word("玊烨")
+
+    @staticmethod
+    def cut(string: str) -> List[str]:
+        return jieba.lcut(string)  # 精确模式
+
+    @staticmethod
+    def cut_for_search(string: str) -> List[str]:
+        return jieba.lcut_for_search(string)
---
+++ personal_tool/ai/NLP/nlp/feature/cut/jieba_feature.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from .entity.jieba_cut import JiebaCut
+
+
+class JiebaFeature:
+    _jieba_cut = None
+
+    @classmethod
+    def cut(cls, string: str) -> List[str]:
+        return cls._get_jieba_cut().cut(string)
+
+    @classmethod
+    def _get_jieba_cut(cls) -> JiebaCut:
+        if cls._jieba_cut is None:
+            cls._jieba_cut = JiebaCut()
+        return cls._jieba_cut
