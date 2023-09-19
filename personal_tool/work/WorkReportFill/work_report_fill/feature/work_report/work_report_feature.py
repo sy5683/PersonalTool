9e7bd2a3cd44e99187ffc6355272bceb4c75a900
玊烨 <207279604@qq.com>
2023-09-19 14:01:35 +0800

    整理公共组件

--- personal_tool/work/WorkReportFill/work_report_fill/feature/work_report/work_report_feature.py
+++ personal_tool/work/WorkReportFill/work_report_fill/feature/work_report/work_report_feature.py
@@ -9,7 +9,7 @@ import openpyxl
 
 from .entity.weekly_report import WeeklyReport
 from .factory.daily_report_factory import DailyReportFactory
-from ...util.time_util import TimeUtil
+from ...util.time_util import TimeUtil, TimeFormat
 
 
 class WorkReportFeature:
@@ -17,7 +17,7 @@ class WorkReportFeature:
 
     @classmethod
     def get_now_weekly_report(cls) -> WeeklyReport:
-        now_date = TimeUtil.get_now()
+        now_date = TimeUtil.get_now(TimeFormat.date)
         weekly_reports = cls.get_weekly_reports()
         for weekly_report in reversed(weekly_reports):
             if now_date in weekly_report.date_range:
--- personal_tool/work/WorkReportFill/work_report_fill/util/time_util.py
+++ personal_tool/work/WorkReportFill/work_report_fill/util/time_util.py
@@ -1,4 +1,9 @@
+import copy
+
+from personal_util.time_util import TimeFormat as timeFormat
 from personal_util.time_util import TimeUtil as timeUtil
+
+TimeFormat = copy.deepcopy(timeFormat)
 
 
 class TimeUtil(timeUtil):
