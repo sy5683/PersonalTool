5c58a267413f0f1a322789990a5c81c109de7991
玊烨 <207279604@qq.com>
2023-11-27 09:37:06 +0800

    代码重构

--- personal_tool/code/TagsFactory/tags_factory/feature/pinyin_feature.py
+++
@@ -1,9 +0,0 @@
-import pypinyin
-
-
-class PinyinFeature:
-
-    @staticmethod
-    def chinese_to_object_name(chinese: str) -> str:
-        """中文转英文拼音（没有音标）"""
-        return "_".join(pypinyin.lazy_pinyin(chinese))
