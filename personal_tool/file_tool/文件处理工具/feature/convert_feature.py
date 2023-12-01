a3df5e6b1437ca623758235984d3a3f7b4916dfe
玊烨 <207279604@qq.com>
2023-12-01 15:27:20 +0800

    优化解压公共组件

--- personal_tool/file_tool/文件处理工具/feature/convert_feature.py
+++ personal_tool/file_tool/文件处理工具/feature/convert_feature.py
@@ -4,6 +4,7 @@ import typing
 
 from common_util.code_util.win32_util.win32_util import Win32Util
 from common_util.file_util.excel_util.excel_util import ExcelUtil
+from common_util.file_util.file_util.file_util import FileUtil
 from common_util.file_util.pdf_util.pdf_util import PdfUtil
 from common_util.file_util.word_util.word_util import WordUtil
 
@@ -14,8 +15,8 @@ class ConvertFeature:
     def to_excel(file_paths: typing.Tuple[str, ...]):
         """转换为excel"""
         for file_path in file_paths:
-            _, suffix = os.path.splitext(file_path)
-            if re.match(r"\.doc$|\.docx$", suffix):
+            original_type = FileUtil.get_original_type(file_path)
+            if re.match(r"docx$", original_type):
                 excel_path = WordUtil.word_to_excel(file_path)
             else:
                 continue
@@ -25,10 +26,10 @@ class ConvertFeature:
     def to_image(file_paths: typing.Tuple[str, ...]):
         """转换为图片"""
         for file_path in file_paths:
-            _, suffix = os.path.splitext(file_path)
-            if re.match(r"\.xlsx$", suffix):
+            original_type = FileUtil.get_original_type(file_path)
+            if re.match(r"xlsx$", original_type):
                 image_paths = ExcelUtil.excel_to_images(file_path)
-            elif re.match(r"\.pdf$", suffix):
+            elif re.match(r"pdf$", original_type):
                 image_paths = PdfUtil.pdf_to_images(file_path)
             else:
                 continue
@@ -39,8 +40,8 @@ class ConvertFeature:
         """转换为pdf"""
         image_paths = []
         for file_path in file_paths:
-            _, suffix = os.path.splitext(file_path)
-            if re.match(r"\.png$|\.jpg$", suffix):
+            original_type = FileUtil.get_original_type(file_path)
+            if re.match(r"png$|jpg$", original_type):
                 image_paths.append(file_path)
         if image_paths:
             pdf_path = PdfUtil.images_to_pdf(image_paths)
