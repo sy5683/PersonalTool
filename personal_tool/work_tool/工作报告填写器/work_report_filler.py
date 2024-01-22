b071279f1105ef5a48466406766dcb18999d0813
玊烨 <207279604@qq.com>
2024-01-22 09:21:42 +0800

    优化日报填写工具

--- personal_tool/work_tool/工作报告填写器/work_report_filler.py
+++ personal_tool/work_tool/工作报告填写器/work_report_filler.py
@@ -7,18 +7,15 @@ from feature.work_report.work_report_feature import WorkReportFeature
 class WorkReportFiller:
 
     def __init__(self, target_date: str = str(datetime.datetime.now())):
-        self.target_date = target_date
+        self.weekly_report = WorkReportFeature.get_weekly_report(target_date)
 
     def main(self):
-        # 1) 获取周报
-        weekly_report = WorkReportFeature.get_weekly_report(self.target_date)
-
-        # 2.1) 登录OA
+        # 1) 登录OA
         OaFeature.login_oa()
-        # 2.2) 切换至日报界面
+        # 2) 切换至日报界面
         OaFeature.switch_in_report_page()
-        # 2.3) 填写工作报告
-        OaFeature.fill_report(weekly_report.daily_reports)
+        # 3) 填写工作报告
+        OaFeature.fill_report(self.weekly_report.daily_reports)
 
 
 if __name__ == '__main__':
