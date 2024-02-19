7126658c80f132160e5ca49ed058c14c891bf450
玊烨 <207279604@qq.com>
2024-02-19 14:04:53 +0800

    整理代码

--- test/common_util/system_util/server_util/server_util_test.py
+++ test/common_util/system_util/server_util/server_util_test.py
@@ -4,8 +4,21 @@ from common_util.system_util.server_util.server_util import ServerUtil
 
 class ServerUtilTestCase(TestBase):
 
+    def setUp(self):
+        self.ip = ""
+        self.port = 9222
+        self.username = ""
+        self.password = ""
+
     def test_get_ftp_connect(self):
-        server_connect = ServerUtil.get_server_connect()
+        server_connect = ServerUtil.get_server_connect(ip=self.ip, port=self.port, username=self.username,
+                                                       password=self.password, server_type='FTP')
+        with server_connect:
+            pass
 
     def test_get_sftp_connect(self):
-        server_connect = ServerUtil.get_server_connect()
+        server_connect = ServerUtil.get_server_connect(ip=self.ip, port=self.port, username=self.username,
+                                                       password=self.password, server_type='SFTP')
+
+        with server_connect:
+            pass
