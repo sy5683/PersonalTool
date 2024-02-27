d0665ca2881e4e4a1252103c94605245cbfaae7b
玊烨 <207279604@qq.com>
2024-02-27 16:28:30 +0800

    实现农行回单pdf解析

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/entity/receipt_profile.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/entity/receipt_profile.py
@@ -1,6 +1,13 @@
 import abc
+import sys
 import typing
+from pathlib import Path
 
+import cv2
+import numpy
+
+from common_util.file_util.file_util.file_util import FileUtil
+from common_util.file_util.image_util.image_util import ImageUtil
 from common_util.file_util.pdf_util.pdf_util import PdfUtil
 from .receipt import Receipt
 
@@ -20,3 +27,48 @@ class ReceiptProfile(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def parse_receipt(self):
         """解析回单"""
+
+    def _judge_images(self, *image_names: str, different: float):
+        """比较图片"""
+        judge_images = self.__get_judge_images(*image_names)
+        for image in PdfUtil.get_pdf_images(self.receipt_path):
+            if len(image.shape) == 2:
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+            elif len(image.shape) == 3 and image.shape[2] != 3:
+                continue
+            for judge_image in judge_images:
+                if self.__compare_image(image, judge_image) < different:
+                    return True
+                image = cv2.flip(image, 0)  # 垂直翻转图片
+                if self.__compare_image(image, judge_image) < different:
+                    return True
+        return False
+
+    @staticmethod
+    def __compare_image(image: numpy.ndarray, judge_image: numpy.ndarray) -> float:
+        """对比两张图片的相似度"""
+        # 如果图片为单通道，这将其变为三通道，如果不是单通道或者三通道，说明图片解析失败
+        if len(image.shape) == 2:
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        elif len(image.shape) == 3 and image.shape[2] != 3:
+            return 1
+        if len(judge_image.shape) == 2:
+            judge_image = cv2.cvtColor(judge_image, cv2.COLOR_GRAY2BGR)
+        elif len(judge_image.shape) == 3 and judge_image.shape[2] != 3:
+            return 1
+        height, width = judge_image.shape[:2]
+        # 缩放一致大小
+        image = cv2.resize(image, (width, height), interpolation=cv2.INTER_NEAREST)
+        # 求和每个像素点之间的差异，除以255是为了归一化，之后再求整体差异性
+        return numpy.sum(cv2.absdiff(image, judge_image)) / 255 / width / height
+
+    def __get_judge_images(self, *image_names: str) -> typing.List[numpy.ndarray]:
+        """获取判断图片"""
+        image_dir_path = Path(sys.modules[self.__module__].__file__).parent.joinpath("judge_image")
+        FileUtil.make_dir(image_dir_path)
+        judge_images = []
+        for image_name in image_names:
+            image_path = image_dir_path.joinpath(image_name)
+            assert image_path.exists(), f"判断图片不存在: {image_name}"
+            judge_images.append(ImageUtil.read_opencv_image(image_path))
+        return judge_images
--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/profiles/农业银行/ABC_01_receipt.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/profiles/农业银行/ABC_01_receipt.py
@@ -1,3 +1,8 @@
+import fitz
+
+from common_util.data_util.number_util.number_util import NumberUtil
+from common_util.data_util.time_util.time_util import TimeUtil
+from ...entity.receipt import Receipt
 from ...entity.receipt_profile import ReceiptProfile
 
 
@@ -8,7 +13,29 @@ class ABC01Receipt(ReceiptProfile):
 
     def judge(self) -> bool:
         """判断是否为当前格式"""
-        return False
+        if not self._judge_images("ABC_image_01.png", different=0.3):
+            return False
+        with fitz.open(self.receipt_path) as pdf:
+            if "网上银行电子回单" not in pdf[0].get_text():
+                return False
+        return True
 
     def parse_receipt(self):
         """解析回单"""
+        for pdf_profile in self.pdf_profiles:
+            for table in pdf_profile.tables:
+                receipt = Receipt()
+                receipt.date = TimeUtil.format_time(table.get_row_values(7)[1])  # 日期
+                name_row_values = table.get_row_values(1)
+                if name_row_values[0] == "付款方":
+                    receipt.payer_account_name = table.get_row_values(2)[1]  # 付款人户名
+                    receipt.payer_account_number = name_row_values[2]  # 付款人账号
+                    receipt.payee_account_name = table.get_row_values(2)[3]  # 收款人户名
+                    receipt.payee_account_number = name_row_values[5]  # 收款人账号
+                elif name_row_values[0] == "收款方":
+                    receipt.payer_account_name = table.get_row_values(2)[3]  # 付款人户名
+                    receipt.payer_account_number = name_row_values[5]  # 付款人账号
+                    receipt.payee_account_name = table.get_row_values(2)[1]  # 收款人户名
+                    receipt.payee_account_number = name_row_values[2]  # 收款人账号
+                receipt.amount = NumberUtil.to_amount(table.get_row_values(4)[1])  # 金额
+                self.receipts.append(receipt)
