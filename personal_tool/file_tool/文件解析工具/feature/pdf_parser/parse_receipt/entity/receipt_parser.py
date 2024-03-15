290ea1ca84878f03292e84a60288798ffae41c88
玊烨 <207279604@qq.com>
2024-03-15 14:35:40 +0800

    优化pdf解析时的图片检测方法

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/entity/receipt_parser.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/entity/receipt_parser.py
@@ -67,6 +67,10 @@ class ReceiptParser(metaclass=abc.ABCMeta):
                 continue
             if len(image.shape) == 2:
                 image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+            if image.shape[2] == 4:
+                image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
+            elif image.shape[2] == 3:
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             for judge_image in judge_images:
                 if self._compare_image(image, judge_image) < different:
                     return True
