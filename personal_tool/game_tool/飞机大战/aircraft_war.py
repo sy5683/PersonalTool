0c22dccddd263c541310f10ba9fe17f72605dc1d
玊烨 <207279604@qq.com>
2024-03-19 10:57:28 +0800

    添加学习代码

---
+++ personal_tool/game_tool/飞机大战/aircraft_war.py
@@ -0,0 +1,12 @@
+from common_core.base.tool_base import ToolBase
+
+
+class AircraftWar(ToolBase):
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    aircraft_war = AircraftWar()
+    aircraft_war.main()
