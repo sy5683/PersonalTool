78fc6a30b4dcde4bd402a7e5ae3c215a5ce12dce
玊烨 <207279604@qq.com>
2024-03-22 14:21:14 +0800

    整理代码

--- test/personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_pdf_test.py
+++ test/personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_pdf_test.py
@@ -8,8 +8,11 @@ from personal_tool.file_tool.文件解析工具.feature.pdf_parser.parse_pdf imp
 
 class ParsePdfTestCase(TestBase):
 
+    def setUp(self):
+        self.receipt_path = Path(r"E:\Document\公司文档\先一科技\RPA\场景文档\01_银行回单补扫\回单")
+
     def test_parse_receipt(self):
-        receipt_path = self.get_test_file("回单").joinpath("")
+        receipt_path = self.receipt_path.joinpath("")
         for pdf_path in self.__get_pdf_path(receipt_path):
             result = ParsePdf.parse_receipt(pdf_path)
             for receipt in result.receipts:
