6fc833add194fdd861bab414cb61c3aec1dacf9c
玊烨 <207279604@qq.com>
2024-04-12 15:28:55 +0800

    优化脚本

--- personal_tool/novel_tool/小说爬虫/feature/crawler/crawler_001.py
+++ personal_tool/novel_tool/小说爬虫/feature/crawler/crawler_001.py
@@ -1,7 +1,8 @@
-import logging
 import re
 import typing
 from pathlib import Path
+
+from selenium.common import TimeoutException
 
 from common_util.code_util.selenium_util.selenium_util import SeleniumUtil
 from .base.crawler_base import CrawlerBase
@@ -15,23 +16,29 @@ class Crawler001(CrawlerBase):
     def run(self, **kwargs):
         url_suffix = kwargs['url_suffix']
         SeleniumUtil.open_url(f"https://www.douyinxs.com/{url_suffix.strip('/')}")
+        driver = SeleniumUtil.get_driver()
+        driver.set_page_load_timeout(5)
         with open(self.save_path, "w+") as file:
-            # file.truncate()  # 清空
+            file.truncate()  # 清空
+            temp_contents = []
             while True:
-                try:
-                    next_button = SeleniumUtil.find('//a[@id="next"]')
-                except Exception as e:
-                    logging.warning(f"页面异常，重新刷新: {e}")
-                    SeleniumUtil.get_driver().refresh()
+                while True:
+                    element = SeleniumUtil.find('//article[@id="content"]')
+                    contents = [each for each in re.split(r"\s+", element.get_attribute("innerText")) if each]
+                    if contents != temp_contents:
+                        temp_contents = contents
+                        break
+                next_button = SeleniumUtil.find('//a[@id="next"]')
                 if "下一页" in next_button.get_attribute("innerText"):
                     title = SeleniumUtil.find('//div[@class="bookname"]/h1').get_attribute("innerText")
                     file.write(f"\n\n{title}\n")
                     print(title)
-                content_element = SeleniumUtil.find('//article[@id="content"]').get_attribute("innerText")
-                for each in re.split(r"\s+", content_element):
-                    if not each:
-                        continue
-                    file.write(f"        {each}\n")
+                for content in contents:
+                    file.write(f"        {content}\n")
                 if "书末页" in next_button.get_attribute("innerText"):
                     break
-                next_button.click()
+                try:
+                    next_button.click()
+                except TimeoutException:
+                    # 点击下一页或者下一章时，可能因为频率太快导致页面无法加载，会在这里报错，因此在这里捕捉一下并刷新一下即可
+                    driver.refresh()
