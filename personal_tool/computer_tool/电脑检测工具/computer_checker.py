feeca9db9bcc9ba5c286edf7ac07cad1e97dbf6f
玊烨 <207279604@qq.com>
2024-05-06 10:01:50 +0800

    优化脚本

--- personal_tool/computer_tool/电脑检测工具/computer_checker.py
+++ personal_tool/computer_tool/电脑检测工具/computer_checker.py
@@ -6,10 +6,10 @@ from feature.selenium_checker import SeleniumChecker
 class ComputerChecker(ToolBase):
 
     def main(self, *args, **kwargs):
-        # 1) 检测谷歌浏览器driver
-        SeleniumChecker.check_chrome_driver()
-        # 2) 检测谷歌浏览器
+        # 1) 检测谷歌浏览器
         BrowserChecker.check_chrome_browser()
+        # 2) 检测谷歌浏览器driver
+        SeleniumChecker.check_chrome_driver()
 
 
 if __name__ == '__main__':
