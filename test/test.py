d022883a8a6b89959b1cf1ef5fa158d1bf05db89
玊烨 <207279604@qq.com>
2024-05-15 11:28:21 +0800

    优化脚本

--- test/test.py
+++ test/test.py
@@ -44,15 +44,3 @@ class Test(TestBase):
         workbook.remove(workbook['Sheet'])
         workbook.save("test.xlsx")
         workbook.close()
-
-    def test_002(self):
-        """云驿燃料调运平台"""
-        # SeleniumUtil.click('//a[text()="对账中心"]')
-        # SeleniumUtil.click('//span[text()="资金流水(账务明细)"]')
-        # SeleniumUtil.click('//a[text()="昨日"]')
-        # SeleniumUtil.click('//span[text()="查 询"]')
-        # SeleniumUtil.click('//span[@class="ant-checkbox-inner"]')
-        # SeleniumUtil.click('//span[text()="批量下载回单"]')
-        SeleniumUtil.click('//button/span[text()="下载文件区"]')
-        SeleniumUtil.find('//div/span[text()="下载文件区"]')
-        SeleniumUtil.click('//div[text()="回单"]')
