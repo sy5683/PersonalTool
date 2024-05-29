40b6cf94be28dc2bd508a1b5bedf2453df8e097f
玊烨 <207279604@qq.com>
2024-05-29 17:36:23 +0800

    优化脚本

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_voucher/parser/微信_商户结算记录/wechat_merchant_voucher.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_voucher/parser/微信_商户结算记录/wechat_merchant_voucher.py
@@ -1,3 +1,11 @@
+import re
+
+import fitz
+
+from common_util.data_util.number_util.number_util import NumberUtil
+from common_util.data_util.time_util.time_util import TimeUtil
+from common_util.file_util.pdf_util.pdf_utils.entity.pdf_element import Table
+from ...entity.voucher import Voucher
 from ...entity.voucher_parser import VoucherParser
 
 
@@ -8,6 +16,41 @@ class WechatMerchantVoucher(VoucherParser):
 
     def judge(self) -> bool:
         """判断是否为当前格式"""
+        with fitz.open(self.voucher_path) as pdf:
+            pdf_text = re.sub(r"\s+", "", pdf[0].get_text())
+            if re.search("微信", pdf_text) and "商户结算记录" in pdf_text:
+                return True
+        return False
 
     def parse_voucher(self):
         """解析凭证"""
+        try:
+            table = self.pdf_profiles[0].tables[0]
+        except IndexError:
+            raise ValueError("无法在商户结算记录中提取出表格数据")
+        tag_row = self.__get_tag_row(table)
+        tags = table.get_row_values(tag_row)
+        for row in range(tag_row + 1, table.max_rows):
+            row_values = table.get_row_values(row)
+            if "合计" in row_values:
+                break
+            data = dict(zip(tags, row_values))
+            voucher = Voucher()
+            voucher.date = TimeUtil.format_to_str(data.get("结算日期"))
+            voucher.date = NumberUtil.to_amount(data.get("订单金额(元)"))
+            voucher.date = data.get("订单笔数")
+            voucher.date = NumberUtil.to_amount(data.get("退款金额(元)"))
+            voucher.date = data.get("退款笔数")
+            voucher.date = NumberUtil.to_amount(data.get("手续费(元)"))
+            voucher.date = NumberUtil.to_amount(data.get("入账金额(元)"))
+            self.vouchers.append(voucher)
+
+
+    @staticmethod
+    def __get_tag_row(table: Table) -> int:
+        """获取表头行"""
+        for row in range(table.max_rows):
+            row_values = table.get_row_values(row)
+            if "结算日期" in row_values and "入账金额(元)" in row_values:
+                return row
+        raise ValueError("无法识别出表格表头")
--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_voucher/parser/支付宝_余额收支流水证明/alipay_balance_voucher.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_voucher/parser/支付宝_余额收支流水证明/alipay_balance_voucher.py
@@ -2,6 +2,7 @@ import re
 
 import fitz
 
+from common_util.data_util.number_util.number_util import NumberUtil
 from common_util.data_util.time_util.time_util import TimeUtil
 from ...entity.voucher import Voucher
 from ...entity.voucher_parser import VoucherParser
@@ -25,19 +26,18 @@ class AlipayBalanceVoucher(VoucherParser):
         try:
             table = self.pdf_profiles[0].tables[0]
         except IndexError:
-            raise ValueError("无法在余额收支流水证明中获取到表格")
-
+            raise ValueError("无法在余额收支流水证明中提取出表格数据")
         tags = table.get_row_values(0)
-        for index in range(1, table.max_rows):
+        for row in range(1, table.max_rows):
+            data = dict(zip(tags, table.get_row_values(row)))
             voucher = Voucher()
-            data = dict(zip(tags, table.get_row_values(index)))
             voucher.date = TimeUtil.format_to_str(data.get("入账时间"))
             voucher.zfbjyh = data.get("支付宝交易号")
             voucher.shddh = data.get("商户订单号")
             voucher.ywlx = data.get("业务类型")
             voucher.dfzhmc = data.get("对方账户名称")
             voucher.dfzfbzh = data.get("对方支付宝账号/银行卡号")
-            voucher.szye = data.get("收支余额")
-            voucher.zhye = data.get("账户余额")
+            voucher.szye = NumberUtil.to_amount(data.get("收支余额"))
+            voucher.zhye = NumberUtil.to_amount(data.get("账户余额"))
             voucher.bz = data.get("备注")
             self.vouchers.append(voucher)
