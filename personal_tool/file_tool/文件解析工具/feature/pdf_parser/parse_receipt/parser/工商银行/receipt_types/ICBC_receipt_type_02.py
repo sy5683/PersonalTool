de86ef0c1fdcaf95e2618556c98c83f3a1b30bc0
玊烨 <207279604@qq.com>
2024-06-04 17:27:45 +0800

    优化pdf解析工具

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/parser/工商银行/receipt_types/ICBC_receipt_type_02.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/parser/工商银行/receipt_types/ICBC_receipt_type_02.py
@@ -19,8 +19,8 @@ class ICBCReceiptType01(ICBCReceiptType):
         receipt = Receipt()
         receipt.date = TimeUtil.format_to_str(self._get_word("^日期[:：](.*)$"))  # 日期
         receipt.payer_account_name = self._get_word("^付款人户名[:：](.*?)$")  # 付款人户名
-        receipt.payer_account_number = self._get_word("^付款人帐号[:：](.*?)$")  # 付款人账号
+        receipt.payer_account_number = self._get_word("^付款人[帐账]号[:：](.*?)$")  # 付款人账号
         receipt.payee_account_name = self._get_word("^收款人户名[:：](.*?)$")  # 收款人户名
-        receipt.payee_account_number = self._get_word("^收款人帐号[:：](.*?)$")  # 收款人账号
+        receipt.payee_account_number = self._get_word("^收款人[帐账]号[:：](.*?)$")  # 收款人账号
         receipt.amount = NumberUtil.to_amount(self._get_word("^小写[:：](.*?)$"))  # 金额
         return receipt
