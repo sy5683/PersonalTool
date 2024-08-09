01b3e68ae359912d6e02e292894b18ddb0561a87
玊烨 <207279604@qq.com>
2024-08-09 15:08:37 +0800

    修改镜像库

--- common_util/code_util/command_util/command_utils/run_cmd/os_cmd.py
+++ common_util/code_util/command_util/command_utils/run_cmd/os_cmd.py
@@ -8,7 +8,7 @@ class OsCmd:
     def install_python_package(requirement_name: str, requirement_version: str, timeout: int):
         """安装python库"""
         cmd_code = f"pip install --default-timeout={timeout}"
-        cmd_code += " -i https://pypi.tuna.tsinghua.edu.cn/simple"
+        cmd_code += " -i https://pypi.doubanio.com/simple"
         cmd_code += f" {requirement_name}"
         cmd_code += f"=={requirement_version}" if requirement_version else ""
         logging.info(f"cmd指令: {cmd_code}")
