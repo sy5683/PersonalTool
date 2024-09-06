5c31c749907cc5f3fa27825bd40141db0f7fb456
玊烨 <207279604@qq.com>
2024-09-06 22:32:23 +0800

    整理代码

--- test/test.py
+++ test/test.py
@@ -28,10 +28,3 @@ class Test(TestBase):
 
     def test_(self):
         """"""
-        from common_util.code_util.temp_util.selenium_util import SeleniumUtil
-        from common_util.code_util.temp_util.selenium_utils.entity.selenium_config import SeleniumConfig
-
-        driver_path = SeleniumUtil.get_chrome_driver_path()
-        SeleniumUtil.open_url(SeleniumConfig(driver_path=driver_path), "https://www.baidu.com/")  # 打开url
-        element = SeleniumUtil.find(SeleniumConfig(xpath='//map[@id="s_mp"]/area', wait_seconds=30))  # 查找元素
-        SeleniumUtil.find(SeleniumConfig(xpath='./ancestor::div[@id="lg"]', element=element))  # 查找父级元素
