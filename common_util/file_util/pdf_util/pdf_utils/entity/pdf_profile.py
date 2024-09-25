6f512213ec6bb8cca155257b6bac9a00136b512f
玊烨 <207279604@qq.com>
2024-09-25 14:39:49 +0800

    整理代码

--- common_util/file_util/pdf_util/pdf_utils/entity/pdf_profile.py
+++ common_util/file_util/pdf_util/pdf_utils/entity/pdf_profile.py
@@ -11,13 +11,6 @@ class PdfProfile:
         self.words: typing.List[Word] = []
 
 
-class ReceiptProfile:
-
-    def __init__(self, table: Table = None, words: typing.List[Word] = None):
-        self.table: typing.Union[Table, None] = table
-        self.words: typing.List[Word] = words if words else []
-
-
 class TableProfile:
     """每页pdf中可能有多个表格或者没有表格，这里以每个表格为一个单元将其进行分割"""
 
