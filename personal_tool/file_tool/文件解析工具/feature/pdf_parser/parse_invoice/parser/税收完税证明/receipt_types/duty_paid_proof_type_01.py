8b70cd501e9b50607a155f1b5b3f2c9138a942ea
玊烨 <207279604@qq.com>
2024-09-26 16:23:26 +0800

    整理代码

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_invoice/parser/税收完税证明/receipt_types/duty_paid_proof_type_01.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_invoice/parser/税收完税证明/receipt_types/duty_paid_proof_type_01.py
@@ -1,3 +1,5 @@
+import re
+
 from common_util.data_util.number_util.number_util import NumberUtil
 from common_util.data_util.time_util.time_util import TimeUtil
 from .duty_paid_proof_type import DutyPaidProofType
@@ -8,8 +10,8 @@ class DutyPaidProofType01(DutyPaidProofType):
 
     def judge(self) -> bool:
         """判断是否为当前格式"""
-        for key in ["原凭证号", "税种", "品目名称", "税款所属时期", "入（退）库日期", "实缴（退）金额"]:
-            if key not in "".join(self.table.get_row_values(1)):
+        for key in ["原凭证号", "税种", "品目名称", "税款所属时期", "入[(（]退[)）]库日期", "实缴[(（]退[)）]金额"]:
+            if not re.search(key, "".join(self.table.get_row_values(1))):
                 return False
         return True
 
