f8cb5d8d8d6e39bd3e0657a498d628f5f4dc4475
玊烨 <207279604@qq.com>
2024-09-27 14:17:29 +0800

    整理代码

--- personal_tool/computer_tool/电脑备份工具/feature/backuper/browser_backuper.py
+++ personal_tool/computer_tool/电脑备份工具/feature/backuper/browser_backuper.py
@@ -14,7 +14,7 @@ class BrowserBackuper:
         if not os.path.exists(user_data_path):
             logging.warning("未找到谷歌浏览器User Data文件路径")
             return
-        bookmark_path = os.path.join(user_data_path, "Bookmarks")
-        BackupFeature.backup_file(bookmark_path, "Chrome书签")
-        bookmark_path = os.path.join(user_data_path, "Bookmarks.bak")
-        BackupFeature.backup_file(bookmark_path, "Chrome书签")
+        # 保存文件
+        for bookmark_name in ["Bookmarks", "Bookmarks.bak"]:
+            bookmark_path = os.path.join(user_data_path, bookmark_name)
+            BackupFeature.backup_file(bookmark_path, "Chrome书签")
