3e8a91b3fa42b07080b5f9aa32f3853b961c399e
玊烨 <207279604@qq.com>
2024-11-18 09:06:49 +0800

    处理flask报错的问题

--- personal_tool/code_tool/国内地区数据/feature/region_service.py
+++ personal_tool/code_tool/国内地区数据/feature/region_service.py
@@ -3,6 +3,7 @@ import typing
 import flask
 import flask_cors
 
+from common_util.interface_util.flask_util.entity.flask_response import FlaskResponse
 from common_util.interface_util.flask_util.flask_util import FlaskUtil
 from .region_feature import RegionFeature
 
@@ -13,12 +14,13 @@ class RegionService:
     @classmethod
     def set_route(cls, app: flask.app):
         """设置接口"""
+
         # 获取详细地区数据
         @app.route(f"/{cls.service_name}/get_detail_regions", methods=["GET", "POST"])
         @flask_cors.cross_origin()
-        def get_detail_regions() -> typing.List[str]:
+        def get_detail_regions() -> FlaskResponse:
             region_info = FlaskUtil.get_kwarg("region_info")
             regions = RegionFeature.get_detail_regions(region_info)
             if not regions:
                 raise ValueError(f"未找到指定的地区信息: {region_info}")
-            return [region.detail for region in regions]
+            return FlaskResponse(data=[region.detail for region in regions])
