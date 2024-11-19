a47f8ee41a246c0c8ddac53a22ae221cf4601f48
玊烨 <207279604@qq.com>
2024-11-19 15:53:30 +0800

    优化框架代码

--- common_core/base/service_base.py
+++ common_core/base/service_base.py
@@ -9,8 +9,9 @@ from .log_base import LogBase
 class ServiceBase(LogBase, metaclass=abc.ABCMeta):
 
     def __init__(self):
-        sub_service_path = Path(sys.modules[self.__module__].__file__)
-        self.app = FlaskUtil.get_app(sub_service_path.parent.name)
+        super().__init__()
+        service_name = Path(sys.modules[self.__module__].__file__).parent.name
+        self.app = FlaskUtil.get_app(service_name)
 
     @abc.abstractmethod
     def set_route(self, *args, **kwargs):
