dee4daa82a220c777bc6c65729bb71f8bae4f236
玊烨 <207279604@qq.com>
2025-01-20 15:02:37 +0800

    实现宁波银行回单解析

--- personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/parser/宁波银行/receipt_types/NBCB_receipt_type_01.py
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/parser/宁波银行/receipt_types/NBCB_receipt_type_01.py
@@ -1,4 +1,5 @@
 import re
+import typing
 
 from common_util.data_util.number_util.number_util import NumberUtil
 from common_util.data_util.time_util.time_util import TimeUtil
@@ -19,47 +20,37 @@ class NBCBReceiptType01(NBCBReceiptType):
         receipt = Receipt()
         receipt.type = self.__str__()  # 类型
         receipt.bank = self.bank_name  # 银行
-        receipt.serial_number = self.table.get_cell(5, 3).get_value()  # 流水号
+        receipt.serial_number = self._get_cell_relative("交易流水").get_value()  # 流水号
         receipt.date = TimeUtil.format_to_str(self._get_word("^.*年.*月.*日$"))  # 日期
         # 从备注信息里提取用户信息
-        remarks = self.table.get_cell(6, 1).get_value("\t").replace("：", ":")
-        payer_account_name_pattern = re.compile("^付款人户名[:：]")
-        payer_account_number_pattern = re.compile("^付款人账号[:：]")
-        payer_account_bank_pattern = re.compile("^付款行行名[:：]")
-        payee_account_name_pattern = re.compile("^收款人户名[:：]")
-        payee_account_number_pattern = re.compile("^收款人账号[:：]")
-        payee_account_bank_pattern = re.compile("^收款行行名[:：]")
-        for remark in re.split(r"\s+", remarks):
-            if payer_account_name_pattern.search(remark):
-                receipt.payer_account_name = payer_account_name_pattern.sub("", remark)  # 付款人户名
-            if payer_account_number_pattern.search(remark):
-                receipt.payer_account_number = payer_account_number_pattern.sub("", remark)  # 付款人账号
-            if payer_account_bank_pattern.search(remark):
-                receipt.payer_account_bank = payer_account_bank_pattern.sub("", remark)  # 付款人开户银行
-            if payee_account_name_pattern.search(remark):
-                receipt.payee_account_name = payee_account_name_pattern.sub("", remark)  # 收款人户名
-            if payee_account_number_pattern.search(remark):
-                receipt.payee_account_number = payee_account_number_pattern.sub("", remark)  # 收款人账号
-            if payee_account_bank_pattern.search(remark):
-                receipt.payee_account_bank = payee_account_bank_pattern.sub("", remark)  # 收款人开户银行
-        # 获取表格中的用户信息
-        account_name = self._get_cell_relative("账号").get_value()
-        account_number = self._get_cell_relative("户名").get_value()
-        account_bank = self._get_cell_relative("开户银行").get_value()
-        # 根据之前备注信息中的用户信息，将表格中的用户信息补充到另一方
-        if receipt.payee_account_name or receipt.payee_account_number or receipt.payee_account_bank:
-            receipt.payer_account_name = account_name
-            receipt.payer_account_number = account_number
-            receipt.payer_account_bank = account_bank
-        elif receipt.payer_account_name or receipt.payer_account_number or receipt.payer_account_bank:
-            receipt.payee_account_name = account_name
-            receipt.payee_account_number = account_number
-            receipt.payee_account_bank = account_bank
+        for remark in re.split(r"\s+", self.table.get_cell(6, 1).get_value("\t")):
+            receipt.payer_account_name = self.get_remark_value("^付款人户名[:：]", remark)  # 付款人户名
+            receipt.payer_account_number = self.get_remark_value("^付款人账号[:：]", remark)  # 付款人账号
+            receipt.payer_account_bank = self.get_remark_value("^付款行行名[:：]", remark)  # 付款人开户银行
+            receipt.payee_account_name = self.get_remark_value("^收款人户名[:：]", remark)  # 收款人户名
+            receipt.payee_account_number = self.get_remark_value("^收款人账号[:：]", remark)  # 收款人账号
+            receipt.payee_account_bank = self.get_remark_value("^收款行行名[:：]", remark)  # 收款人开户银行
+        # 根据回单种类判断收付类型
+        receipt_type = self._get_cell_relative("回单种类").get_value()
+        # 付款回单: 转账支取回单
+        if re.search("支取", receipt_type):
+            receipt.payer_account_name = self._get_cell_relative("账号").get_value()  # 付款人户名
+            receipt.payer_account_number = self._get_cell_relative("户名").get_value()  # 付款人账号
+            receipt.payer_account_bank = self._get_cell_relative("开户银行").get_value()  # 付款人开户银行
+        # 收款回单: 转账存入回单
+        elif re.search("存入", receipt_type):
+            receipt.payee_account_name = self._get_cell_relative("账号").get_value()  # 收款人户名
+            receipt.payee_account_number = self._get_cell_relative("户名").get_value()  # 收款人账号
+            receipt.payee_account_bank = self._get_cell_relative("开户银行").get_value()  # 收款人开户银行
         else:
-            business_type = self.table.get_cell(5, 1).get_value()  # 业务种类
-            # TODO 根据业务种类判断收付款类型，判断逻辑待定
+            raise TypeError(f"宁波银行无法判断的回单种类，请联系开发增加解析格式: {receipt_type}")
         amount = self.table.get_cell(3, 1).get_value()
         receipt.amount = NumberUtil.to_amount(amount.split("RMB")[-1] if "RMB" in amount else amount)  # 金额
         receipt.abstract = ""  # 摘要
         receipt.image = self.image  # 图片
         return receipt
+
+    @staticmethod
+    def get_remark_value(pattern: str, value: str) -> typing.Union[str, None]:
+        if re.search(pattern, value):
+            return re.sub(pattern, "", value)
