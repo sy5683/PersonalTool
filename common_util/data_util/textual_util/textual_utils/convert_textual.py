684def3bd3ff196d1b3f25a3829a0b8ed742c230
玊烨 <207279604@qq.com>
2025-02-11 11:38:42 +0800

    优化unicode编码解析公共组件，处理测试中发现的无法解析的格式

--- common_util/data_util/textual_util/textual_utils/convert_textual.py
+++ common_util/data_util/textual_util/textual_utils/convert_textual.py
@@ -1,7 +1,12 @@
 import re
 import typing
 from urllib import parse
+from enum import Enum
 
+
+class DecodeType(Enum):
+    unicode = "unicode"
+    utf_8 = "utf_8"
 
 class ConvertTextual:
 
@@ -10,30 +15,43 @@ class ConvertTextual:
         """文本解码"""
         # 1) 格式化文本
         textual = cls._format_textual(textual)
-        # 2) 根据文本类型转码
-        if re.match(r"\\u", textual):
+        # 2) 判断文本转码类型
+        decode_types = []
+        if re.search(r"\\u", textual):
+            decode_types.append(DecodeType.unicode)
+        if re.match(r"\\x", textual):
+            decode_types.append(DecodeType.utf_8)
+        # 3) 根据文本类型转码
+        if len(decode_types) > 1:
+            raise TypeError("文本异常，有多种格式的转码类型，无法解析")
+        elif decode_types == [DecodeType.unicode]:
             return cls._unicode_to_str(textual)
-        elif re.match(r"\\x", textual):
+        elif decode_types == [DecodeType.utf_8]:
             return cls._utf_8_to_str(textual)
         else:
-            return textual
+            raise TypeError("暂不支持的文本解码类型")
 
     @staticmethod
     def _format_textual(textual: typing.Union[bytes, str]) -> str:
         """格式化文本"""
-        # bytes类型转码为字符串
+        # 1) bytes类型转码为字符串
         if isinstance(textual, bytes):
             textual = textual.decode()
-        # 将文本中的单斜杠统一转换为双斜杠
+        # 2) 将文本中的单斜杠统一转换为双斜杠
         return textual.encode("raw_unicode_escape").decode()
 
     @staticmethod
     def _unicode_to_str(unicodes: str) -> str:
         """unicode转字符串"""
-        # 接收的unicode中特殊字符可能会出现不足四位的情况，因此使用正则表达式在其前方补零
+        # 1) 接收的unicode中特殊字符可能会出现不足四位的情况，因此使用正则表达式在其前方补零
         for unicode in list(set(re.findall(r"(\\u\w{1,3})(?=[\\<])", unicodes))):
             code = unicode.replace("\\u", "")
             unicodes = unicodes.replace(unicode, unicode.replace(f"\\u{code}", f"\\u{code.zfill(4)}"))
+        # 2) 特殊处理一些无法在xmltodict方法中使用的字符串
+        # 正则批量去除无法解码的字符串
+        unicodes = re.sub(r"\\u0000", "", unicodes)
+        # 【<】无法使用，替换为【《】（【>】可以使用，但是为了美观，也替换为【》】）
+        unicodes = unicodes.replace("\\u003c", "\\u300a").replace("\\u003e", "\\u300b")
         return unicodes.encode('utf-8').decode('unicode_escape')
 
     @staticmethod
--- test/common_util/data_util/textual_util/textual_util_test.py
+++ test/common_util/data_util/textual_util/textual_util_test.py
@@ -1,3 +1,7 @@
+import xml
+
+import xmltodict
+
 from common_core.base.test_base import TestBase
 from common_util.data_util.textual_util.textual_util import TextualUtil
 
@@ -9,7 +13,22 @@ class TextualUtilTestCase(TestBase):
         self.textual = "\\u738A\\u70E8"
         # self.textual = "\xe7\x8e\x8a\xe7\x83\xa8"
         # self.textual = b"\\xe7\\x8e\\x8a\\xe7\\x83\\xa8"
+        self.response_string = """"""
 
     def test_textual_decode(self):
         result_textual = TextualUtil.textual_decode(self.textual)
         print(result_textual)
+
+    def test_check_error(self):
+        split_key = "INFO"
+        split_strings = self.response_string.split(f"</{split_key}><{split_key}>")
+        for index, response_string in enumerate(split_strings):
+            if index != 0:
+                response_string = f"<{split_key}>{response_string}"
+            if index != len(split_strings) - 1:
+                response_string = f"{response_string}</{split_key}>"
+            result_string = TextualUtil.textual_decode(response_string)
+            try:
+                xmltodict.parse(result_string)
+            except xml.parsers.expat.ExpatError:
+                print(f"【{index}】\n{result_string}")
