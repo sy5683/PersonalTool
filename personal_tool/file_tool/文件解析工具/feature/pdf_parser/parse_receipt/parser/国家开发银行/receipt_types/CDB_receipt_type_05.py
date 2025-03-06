946598901b801d5a425598118993db36a70d8ee3
玊烨 <207279604@qq.com>
2025-03-06 16:18:03 +0800

    添加国开行新格式回单解析

---
+++ personal_tool/file_tool/文件解析工具/feature/pdf_parser/parse_receipt/parser/国家开发银行/receipt_types/CDB_receipt_type_05.py
@@ -0,0 +1,34 @@
+import re
+
+from common_util.data_util.number_util.number_util import NumberUtil
+from common_util.data_util.time_util.time_util import TimeUtil
+from .CDB_receipt_type import CDBReceiptType
+from ....entity.receipt import Receipt
+
+
+class CDBReceiptType05(CDBReceiptType):
+
+    def judge(self) -> bool:
+        """判断是否为当前格式"""
+        for key in ["项目名称", "工本费/转账汇款手续费/手续费", "金额"]:
+            if not re.search(key, "".join(self.table.get_row_values(1))):
+                return False
+        if "合计金额" not in self.table.get_col_values(0):
+            return False
+        return True
+
+    def get_receipt(self) -> Receipt:
+        """解析"""
+        receipt = Receipt()
+        receipt.type = self.__str__()  # 类型
+        receipt.bank = self.bank_name  # 银行
+        receipt.date = TimeUtil.format_to_str(self._get_word("^.*年.*月.*日$"))  # 日期
+        receipt.receipt_number = self._get_word(r"^[a-zA-Z\d+]+$")  # 回单编号
+        receipt.serial_number = self._get_word("^流水号[:：](.*?)$")  # 流水号
+        payer_account_row_cells = self.table.get_row_cells(0)
+        receipt.payer_account_name = self._get_name(payer_account_row_cells[0].get_value())  # 付款人户名
+        receipt.payer_account_number = self._get_account(payer_account_row_cells[1].get_value())  # 付款人账号
+        receipt.amount = NumberUtil.to_amount(self._get_cell_relative("^合计金额", 2).get_value())  # 金额
+        receipt.abstract = ""  # 摘要
+        receipt.image = self.image  # 图片
+        return receipt
